3a82166e49f80e11c20454d4e30717cd49b31ff5
AdriaLopezGonzalez <alopezg@edu.tecnocampus.cat>
2022-04-26 17:40:53 +0100

    cambios

---
+++ Assets/scripts/forces.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forces : MonoBehaviour
+{
+    Rigidbody2D _rigidbody;
+
+    public Transform Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        AddForceToPlayer();
+    }
+
+    void AddForceToPlayer()
+    {
+        Vector2 dir = Player.position - transform.position;
+        var magnitud = dir.magnitude;
+        dir = dir.normalized;
+        dir *= magnitud;
+        // _rigidbody.AddForce(dir);
+        _rigidbody.velocity = dir;
+    }
+}
---
+++ Assets/scripts/inputFisics.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class inputFisics : MonoBehaviour
+{
+    Rigidbody2D _rigidbody;
+
+    float _horizontal;
+    float _vertical;
+
+    public float Speed = 3;
+    public float ForceSpeed = 1;
+
+    public float MaxSpeed;
+
+    [Range(0, 1)]
+    public float Responsifness;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log(_horizontal + " / " + _vertical);
+    }
+
+    void MoveWithVelocity()
+    {
+        Vector2 dir = new Vector2(_horizontal, -_vertical) * Speed;
+        
+        Vector2 targetVelocity = dir;
+
+        _rigidbody.velocity = Vector2.Lerp(_rigidbody.velocity, targetVelocity, Responsifness);
+    }
+
+    void MoveWithForce()
+    {
+        Vector2 dir = new Vector2(_horizontal, -_vertical) * ForceSpeed;
+        _rigidbody.AddForce(dir);
+
+        Vector2 vel = _rigidbody.velocity;
+        Vector2 normVelocity = vel.normalized;
+
+        float speed = Mathf.Min(vel.magnitude, MaxSpeed);
+
+        _rigidbody.velocity = normVelocity * speed;
+    }
+    private void OnMove(InputValue inputValue)
+    {
+        _horizontal = inputValue.Get<Vector2>().x;
+        _vertical = inputValue.Get<Vector2>().y;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+}
